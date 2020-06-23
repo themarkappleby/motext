c0a16fbb4712f9b35aa2b1c19c22f52438d2e357
themarkappleby <themarkappleby@gmail.com>
2020-06-23 17:18:15 -0400

    removed old code for multiple targets

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 /* global motext */
 
 motext('#singleTarget', tl => tl.play())
-
-motext('.multiTarget', tl => tl.play(), {
-  colors: ['red', 'green', 'yellow'],
-  revealProperty: 'scale',
-  revealAmount: 0,
-  staggerAmount: 0.2
-})
