1ac24ced4aa96cc6b11294152a1c360ce63e768f
themarkappleby <themarkappleby@gmail.com>
2020-07-26 17:57:57 -0400

    added prepend polyfill for IE 11

--- src/motext.js
+++ src/motext.js
@@ -1,4 +1,4 @@
-/* global fetch Node HTMLElement, NodeList */
+/* global fetch Node HTMLElement NodeList Element Document DocumentFragment */
 
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
@@ -30,7 +30,33 @@ const SYMBOL_MAP = {
   '"': 'double-quote',
   '\'': 'apostrophe',
   '&': 'ampersand'
-}
+};
+
+// prepend() polyfill for IE 11
+// Source: https://github.com/jserz/js_piece/blob/master/DOM/ParentNode/prepend()/prepend().md
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('prepend')) { // eslint-disable-line
+      return
+    }
+    Object.defineProperty(item, 'prepend', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function prepend () {
+        var argArr = Array.prototype.slice.call(arguments)
+        var docFrag = document.createDocumentFragment()
+
+        argArr.forEach(function (argItem) {
+          var isNode = argItem instanceof Node
+          docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)))
+        })
+
+        this.insertBefore(docFrag, this.firstChild)
+      }
+    })
+  })
+})([Element.prototype, Document.prototype, DocumentFragment.prototype])
 
 const DESCENDERS = ['Q', 'g', 'j', 'p', 'q', 'y', ',']
 const ASCENDERS = ['"', '\'']
